b2c50d1168165e62cfc07e30a4fdb66f594ca891
Marcel1991 <marclerios91@gmail.com>
2022-01-11 09:54:08 -0500

    Se sube el proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+
+  templateUrl: './app.component2.html',
+  styleUrls: ['./app.component.scss']
+
+
+
+})
+export class AppComponent {
+  title = 'ejemplo1';
+}
